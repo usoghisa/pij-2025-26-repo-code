f0f720f1a63a52b6a5a7f920adc0731c0a1b291a
Carsten Fuhs <fuhs@informatik.rwth-aachen.de>
2025-10-21 21:12:33 +0100

    Day 6 code for exercises

---
+++ src/Day06/pij/day06/phones/PhoneLauncher.java
@@ -0,0 +1,24 @@
+package pij.day06.phones;
+
+/**
+ * Exercises some of the functionality in the OldPhone hierarchy.
+ */
+public class PhoneLauncher {
+
+    /**
+     * Launches the PhoneLauncher to exercise the OldPhone hierarchy.
+     */
+    public void launch() {
+        // your code creating and using SmartPhone here...
+    }
+
+    /**
+     * Creates and launches a PhoneLauncher.
+     *
+     * @param args ignored
+     */
+    public static void main(String[] args) {
+        PhoneLauncher launcher = new PhoneLauncher();
+        launcher.launch();
+    }
+}
